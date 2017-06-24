e600c995d3f5398af89278a464b45567e99fe01d
Puneet <puneetkumar.singh@thomsonreuters.com>
2017-06-24 08:42:25 -0400

    added logic to open modal popup in parrent div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
 import { TaskService } from './task/task.service';
-import { LogService } from './error/error.service';
+import { LogService } from './log.service';
 
 @Component({
   selector: 'app-root',
